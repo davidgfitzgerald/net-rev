ad01c138fe346537627b48c5c42e1550b4d14db8
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2020-12-23 13:01:49 +0000

    Empty test

--- src/components/questions/question.test.js
+++ src/components/questions/question.test.js
@@ -1,33 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import Enzyme, { shallow, mount } from 'enzyme';
-import toJson from "enzyme-to-json";
-import Adapter from 'enzyme-adapter-react-16';
-import { unmountComponentAtNode, render, cleanup } from 'react-dom'
-import { act } from "@testing-library/react";
+describe("Question", () => {
+  it('should ', function () {
 
-Enzyme.configure({adapter: new Adapter()})
-
-import Question from "./question";
-import QuestionContainer from "./question_container";
-
-let container = null;
-beforeEach(() => {
-  container = document.createElement("div")
-  document.body.appendChild(container)
+  });
 })
-
-afterEach(() => {
-  unmountComponentAtNode(container)
-  container.remove();
-  container = null;
-})
-
-test.only("QuestionContainer renders (enzyme)",() => {
-  const qContainer = render(<QuestionContainer/>, container)
-  const wrapper = mount(qContainer)
-  expect(toJson(wrapper)).toMatchSnapshot()
-  // expect(qContainer).toBeTruthy()
-  // console.dir(qContainer)
-})
-
